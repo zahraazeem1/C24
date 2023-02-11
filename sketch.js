598c71e87f5234542b282a5ef7ec197572062bb5
zahraazeem1 <114787158+zahraazeem1@users.noreply.github.com>
2023-02-10 18:40:59 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var array1 = [3,4,5,6]
+var array2 = [[7,8,9],[10,11,12],[13,14,15]]
+console.log(array1)
+console.log(array1[0])
+console.log(array1[1])
+console.log(array2)
+console.log(array2[0])
+console.log(array2[1])
+console.log(array2[0][1])
+console.log(array2[1][2])
+console.log(array2[2][2])
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var balls = [];
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+
+  cannon = new Cannon(180, 110, 130, 100, angle);
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+ 
+
+  push();
+  imageMode(CENTER);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i]);
+  }
+
+  cannon.display();
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    cannonBall.trajectory = [];
+    Matter.Body.setAngle(cannonBall.body, cannon.angle);
+    balls.push(cannonBall);
+  }
+}
+
+function showCannonBalls(ball) {
+  if (ball) {
+    ball.display();
+  }
+}
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    balls[balls.length - 1].shoot();
+  }
+}
